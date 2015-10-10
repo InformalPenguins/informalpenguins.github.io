4e8ede4ecf05da4af0e6092977b4c99c8311cbdb
tonymtz <tmartinez@switchfly.com>
2015-10-10 14:17:47 -0500

    fix for touch

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 (function($) {
     $(function() {
-        $('.button-collapse').sideNav({
-            // menuWidth: 300, // Default is 240
-            edge: 'right', // Choose the horizontal origin
-            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        });
+        $('.button-collapse').sideNav();
     });
 })(jQuery);
